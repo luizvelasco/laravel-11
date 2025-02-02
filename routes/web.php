4bcae604af81c008e42fc5f090fc5092bbb46cac
luizvelasco <luizvelasco@gmail.com>
2025-02-02 11:36:21 -0300

    Eloquent - Buscando registros

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,34 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+
+    // Salvando registros, forma 1
+    // $post = new Post();
+    // $post->title = 'Meu primeiro post';
+    // $post->body = 'texto texto';
+    // $post->save();
+
+    // Salvando registros, forma 2
+    // $post = Post::create([
+    //     'title' => 'Meu segundo post',
+    //     'body' => 'texto aqui'
+    // ]);
+
+    // buscando registros forma 1
+    //$post = Post::find(2);
+    
+    // buscando registros forma 2
+    //$post = Post::where('title', 'meu segundo post')->first();
+
+    // buscando registros forma 3
+    $post = Post::where('title', 'LIKE', '%post%')->get();
+    
+    dd($post);
+    //return view('welcome');
 });
 
 Route::get('admin/usuarios', [UserController::class, 'index']);
